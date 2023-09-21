9e0c3435d792a8bdf8c4a2d8831b5a03ea103004
onebeastchris <github@onechris.mozmail.com>
2023-09-21 20:28:44 +0200

    Cleanup, no need to try and add existing permissions

--- src/main/java/net/onebeastchris/extension/geyserpermissionfix/GeyserRegisterPermissionsEventImpl.java
+++ src/main/java/net/onebeastchris/extension/geyserpermissionfix/GeyserRegisterPermissionsEventImpl.java
@@ -4,37 +4,44 @@ import net.luckperms.api.LuckPerms;
 import net.luckperms.api.LuckPermsProvider;
 import net.luckperms.api.model.data.DataMutateResult;
 import net.luckperms.api.model.group.Group;
+import net.luckperms.api.node.NodeEqualityPredicate;
 import net.luckperms.api.node.types.PermissionNode;
+import net.luckperms.api.util.Tristate;
 import org.geysermc.geyser.api.event.lifecycle.GeyserRegisterPermissionsEvent;
 import org.geysermc.geyser.api.util.TriState;
 
 import static net.onebeastchris.extension.geyserpermissionfix.GeyserPermissionFix.logger;
 
 public class GeyserRegisterPermissionsEventImpl implements GeyserRegisterPermissionsEvent {
-
     private final LuckPerms luckperms;
+    private final Group group;
 
     public GeyserRegisterPermissionsEventImpl() {
         this.luckperms = LuckPermsProvider.get();
+        this.group = luckperms.getGroupManager().getGroup("default");
+        if (group == null) {
+            logger.warning("Unable to find default group to add permissions to!");
+        }
     }
 
     @Override
     public void register(String permission, TriState defaultValue) {
-        PermissionNode.Builder node = PermissionNode
+        PermissionNode node = PermissionNode
                 .builder(permission)
-                .value(Boolean.TRUE.equals(defaultValue.toBoolean()));
+                .value(Boolean.TRUE.equals(defaultValue.toBoolean()))
+                .build();
 
-        Group group = luckperms.getGroupManager().getGroup("default");
-        if (group != null) {
-            DataMutateResult result = group.data().add(node.build());
-            if (!result.wasSuccessful()) {
-                if (result.equals(DataMutateResult.FAIL_ALREADY_HAS)) {
-                    // Permission already exists, no need to log
-                    return;
-                }
-                logger.warning("Failed to register permission " + permission + " for default group due to " + result.name() + " !");
-            }
-            luckperms.getGroupManager().saveGroup(group);
+        Tristate tristate = group.data().contains(node, NodeEqualityPredicate.ONLY_KEY);
+        if (!tristate.equals(Tristate.UNDEFINED)) {
+            // Permission already exists as either true/false; no need to add it
+            return;
         }
+
+        DataMutateResult result = group.data().add(node);
+        if (!result.wasSuccessful()) {
+            logger.warning("Failed to register permission " + permission + " for default group due to " + result.name() + " !");
+        }
+
+        luckperms.getGroupManager().saveGroup(group);
     }
 }
